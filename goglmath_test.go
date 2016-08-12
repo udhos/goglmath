dc3a8e6eb3805694af3ca5dc7486000b0ee04b68
Everton Marques <everton.marques@gmail.com>
2016-08-12 10:36:20 -0300

    Test matrix copy.

--- goglmath_test.go
+++ goglmath_test.go
@@ -13,6 +13,20 @@ func TestNormalize3(t *testing.T) {
 	}
 }
 
+func TestCopy(t *testing.T) {
+	m1 := &Matrix4{}
+	m2 := &Matrix4{}
+	for i := 0; i < 16; i++ {
+		m1.data[i] = float32(i)
+	}
+	m2.copyFrom(m1)
+	for i := 0; i < 16; i++ {
+		if m1.data[i] != m2.data[i] {
+			t.Errorf("matrix copy fail: position %d: expected=%v got=%v", i, m1.data[i], m2.data[i])
+		}
+	}
+}
+
 func BenchmarkSetNullMatrix(b *testing.B) {
 	m := &Matrix4{}
 	//m.Malloc()
